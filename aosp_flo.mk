5615dd7e34f74037378c5558a708d507310f73ac
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-26 19:10:06 +0900

    Add makefiles of device/aosp and vendor/aosp

--- aosp_flo.mk
+++ aosp_flo.mk
@@ -32,3 +32,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := true
 $(call inherit-product, device/asus/flo/device.mk)
 $(call inherit-product-if-exists, vendor/asus/flo/device-vendor.mk)
 #$(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-vendor.mk)
+
+#AOSP
+$(call inherit-product-if-exists, vendor/aosp/asus/flo/aosp.mk)
+$(call inherit-product, device/aosp/common/aosp.mk)
